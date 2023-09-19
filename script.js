cadf6023ca3e17daaa05aa1990d33397d8c68d72
Kyle <notxap55@yahoo.com>
2023-09-19 16:45:54 -0700

    working

--- script.js
+++ script.js
@@ -7,21 +7,31 @@ function checkCashRegister(price, cash, cid) {
   let cashDue = cash - price
 
   if (cashDue > 0) {
-    // loop backwards over text values in cid
     let currentDrawerItem = []
-    // if cid item value <= cashDue
+    // loop backwards over text values in cid
+    for (let i = cid.length - 1; i >= 0; i--) {
+      let drawerItemValue = cid[i][1]
+      const matrixValue = matrix[cid[i][0]]
+      if (matrixValue <= cashDue && matrixValue <= drawerItemValue) {
+        // subtract value from cashdue
+        drawerItemValue -= matrixValue
+
+        console.log(drawerItemValue);
+        // subtract value from arrayValue
+        // i++
+
+      }
+      // if cid item value <= cashDue
+      // console.log(matrix[element[0]]);
+      // if (matrix[element[0]])
+    }
     // currentDrawerItem.push(cid item[i][0])
     // currentDrawerItem.push(cid item[i])
     // cid price += cid item matrix value
     // 
-
-
   }
-  message.status = 'OPEN';
-  message.change = change
-
-  console.log(cashDue);
+  // console.log('cash due: ', cashDue);
   return message
 }
 
-console.log(checkCashRegister(20, 50, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+console.log(checkCashRegister(20, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
